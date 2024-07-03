e0cf433659e9f5615306083b51c07961c153c71e
Behnam Sepehri <behnam.sep@gmail.com>
2024-07-03 12:58:49 +0200

    Added new CSS styles for better table presentation and alignment

--- src/component/UserList.tsx
+++ src/component/UserList.tsx
@@ -1,27 +1,40 @@
 import React from 'react';
 
 interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  city: string;
-  age: number;
+	id: number;
+	firstName: string;
+	lastName: string;
+	city: string;
+	age: number;
 }
 
 interface UserListProps {
-  users: User[];
+	users: User[];
 }
 
 const UserList: React.FC<UserListProps> = ({ users }) => {
-  return (
-    <ul>
-      {users.map((user) => (
-        <li key={user.id}>
-          {user.firstName} {user.lastName} - {user.city} - {user.age} years old
-        </li>
-      ))}
-    </ul>
-  );
+	return (
+		<table className="table-container">
+			<thead>
+				<tr>
+					<th>Name</th>
+					<th>City</th>
+					<th>Age</th>
+				</tr>
+			</thead>
+			<tbody>
+				{users.map((user) => (
+					<tr key={user.id}>
+						<td>
+							{user.firstName} {user.lastName}
+						</td>
+						<td>{user.city}</td>
+						<td>{user.age}</td>
+					</tr>
+				))}
+			</tbody>
+		</table>
+	);
 };
 
 export default UserList;
