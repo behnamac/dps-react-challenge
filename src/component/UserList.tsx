710844accc704a13d6e97deff973414acde1b294
Behnam Sepehri <behnam.sep@gmail.com>
2024-07-03 14:14:10 +0200

    Replace birthday instead of age

--- src/component/UserList.tsx
+++ src/component/UserList.tsx
@@ -1,40 +1,46 @@
 import React from 'react';
 
 interface User {
-	id: number;
-	firstName: string;
-	lastName: string;
-	city: string;
-	age: number;
+  id: number;
+  firstName: string;
+  lastName: string;
+  city: string;
+  birthDate: string;
 }
 
 interface UserListProps {
-	users: User[];
+  users: User[];
 }
 
+const formatDate = (dateString: string) => {
+  const date = new Date(dateString);
+  const day = date.getDate().toString().padStart(2, '0');
+  const month = (date.getMonth() + 1).toString().padStart(2, '0'); 
+  const year = date.getFullYear();
+  return `${day}.${month}.${year}`;
+};
+
 const UserList: React.FC<UserListProps> = ({ users }) => {
-	return (
-		<table className="table-container">
-			<thead>
-				<tr>
-					<th>Name</th>
-					<th>City</th>
-					<th>Age</th>
-				</tr>
-			</thead>
-			<tbody>
-				{users.map((user) => (
-					<tr key={user.id}>
-						<td>
-							{user.firstName} {user.lastName}
-						</td>
-						<td>{user.city}</td>
-						<td>{user.age}</td>
-					</tr>
-				))}
-			</tbody>
-		</table>
-	);
+  return (
+    <table className="table-container">
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>City</th>
+          <th>Birth Date</th>
+        </tr>
+      </thead>
+      <tbody>
+        {users.map((user) => (
+          <tr key={user.id}>
+            <td>{user.firstName} {user.lastName}</td>
+            <td>{user.city}</td>
+            <td>{formatDate(user.birthDate)}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default UserList;
